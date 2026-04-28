6e674c6d14813a01acb83c1b4d5d659932d92f4b
2bit <i@2bit.jp>
2026-04-28 20:51:07 +0900

    refactor: use nozzle as submodule, build from source instead of prebuilt lib
    Nozzle added as git submodule at libs/nozzle.
    addon_config.mk now compiles nozzle sources directly (common + metal backend).
    plog header-only lib vendored at libs/plog/include/ for nozzle's logging.
    Removed prebuilt lib dependency.

--- addon_config.mk
+++ addon_config.mk
@@ -5,21 +5,49 @@ meta:
 	ADDON_TAGS = "texture" "sharing" "gpu" "syphon" "spout"
 
 common:
-	ADDON_INCLUDES = libs/nozzle/include
+	ADDON_INCLUDES = libs/nozzle/include libs/plog/include
 
 osx:
-	ADDON_CFLAGS = -fno-exceptions -fno-rtti
+	ADDON_CFLAGS = -fno-exceptions -fno-rtti -DNOZZLE_HAS_METAL
 	ADDON_LDFLAGS = -framework Metal -framework IOSurface -framework Foundation -framework OpenGL
-	ADDON_LIBS = libs/nozzle/lib/osx/libnozzle.a
 	ADDON_SOURCES_EXCLUDE = src/%.cpp
-	ADDON_SOURCES = src/ofxNozzleSender.mm src/ofxNozzleReceiver.mm src/ofxNozzleInterop.mm
+	ADDON_SOURCES_EXCLUDE += libs/nozzle/src/backends/d3d11/%
+	ADDON_SOURCES = src/ofxNozzleSender.mm
+	ADDON_SOURCES += src/ofxNozzleReceiver.mm
+	ADDON_SOURCES += src/ofxNozzleInterop.mm
+	ADDON_SOURCES += libs/nozzle/src/common/registry.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/sender.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/receiver.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/frame.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/texture.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/device.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/discovery.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/metadata.cpp
+	ADDON_SOURCES += libs/nozzle/src/c_api/nozzle_c.cpp
+	ADDON_SOURCES += libs/nozzle/src/backends/metal/metal_backend.mm
+	ADDON_SOURCES += libs/nozzle/src/backends/metal/metal_texture.mm
+	ADDON_SOURCES += libs/nozzle/src/backends/metal/metal_sync.mm
 
 macos:
-	ADDON_CFLAGS = -fno-exceptions -fno-rtti
+	ADDON_CFLAGS = -fno-exceptions -fno-rtti -DNOZZLE_HAS_METAL
 	ADDON_LDFLAGS = -framework Metal -framework IOSurface -framework Foundation -framework OpenGL
-	ADDON_LIBS = libs/nozzle/lib/osx/libnozzle.a
 	ADDON_SOURCES_EXCLUDE = src/%.cpp
-	ADDON_SOURCES = src/ofxNozzleSender.mm src/ofxNozzleReceiver.mm src/ofxNozzleInterop.mm
+	ADDON_SOURCES_EXCLUDE += libs/nozzle/src/backends/d3d11/%
+	ADDON_SOURCES = src/ofxNozzleSender.mm
+	ADDON_SOURCES += src/ofxNozzleReceiver.mm
+	ADDON_SOURCES += src/ofxNozzleInterop.mm
+	ADDON_SOURCES += libs/nozzle/src/common/registry.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/sender.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/receiver.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/frame.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/texture.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/device.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/discovery.cpp
+	ADDON_SOURCES += libs/nozzle/src/common/metadata.cpp
+	ADDON_SOURCES += libs/nozzle/src/c_api/nozzle_c.cpp
+	ADDON_SOURCES += libs/nozzle/src/backends/metal/metal_backend.mm
+	ADDON_SOURCES += libs/nozzle/src/backends/metal/metal_texture.mm
+	ADDON_SOURCES += libs/nozzle/src/backends/metal/metal_sync.mm
 
 linux64:
 	ADDON_SOURCES_EXCLUDE = src/%.mm
